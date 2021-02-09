7754f1d76d102dbb08cff51ac0fff3a5e7fc3813
Li Feng <quic_lifeng@quicinc.com>
2021-02-09 01:26:40 -0800

    qcacmn: Add service bit to support backward compatibility
    The host will send TPC power to FW via the new WMI cmd
    WMI_VDEV_SET_TPC_POWER_CMDID going forward instead of
    WMI_VDEV_PARAM_TX_PWRLIMIT to accommodate 6GHz channels.
    But, to support backward compatibility for New Host+old FW
    the TPC power will be sent to FW via legacy WMI cmds
    WMI_VDEV_SET_PARAM_CMDID (during initial connection) or
    WMI_VDEV_PARAM_TX_PWRLIMIT (power value change during RRM
    req/bcn processing from connected AP) with the help of a
    service bit WMI_SERVICE_EXT_TPC_REG_SUPPORT.
    
    Change-Id: I22e11ff847cd384fac8ea0061e5df42cea0c8de8
    CRs-Fixed: 2874245

--- target_if/regulatory/inc/target_if_reg.h
+++ target_if/regulatory/inc/target_if_reg.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2017-2020 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  *
  * Permission to use, copy, modify, and/or distribute this software for
@@ -91,4 +92,13 @@ target_if_regulatory_get_rx_ops(struct wlan_objmgr_psoc *psoc);
  */
 struct wlan_lmac_if_reg_tx_ops *
 target_if_regulatory_get_tx_ops(struct wlan_objmgr_psoc *psoc);
+
+/*
+ * target_if_regulatory_set_ext_tpc() - populate FW support of new WMI command
+ * for TPC
+ * @psoc: psoc pointer
+ *
+ * Return: Success or Failure
+ */
+QDF_STATUS target_if_regulatory_set_ext_tpc(struct wlan_objmgr_psoc *psoc);
 #endif /* __TARGET_IF_REG_H__ */
