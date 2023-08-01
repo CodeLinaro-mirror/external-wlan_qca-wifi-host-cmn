abaea3ba2846de3d747108cfbd066dba0f5aa645
Gururaj Pandurangi <quic_panduran@quicinc.com>
2023-07-31 19:10:30 -0700

    qcacmn: Add support to configure MLD ID in scan request
    In the MLO-MBSSID test, STA can request information of
    non-Tx BSS through Tx BSS by configuring non-Tx BSS MLD
    ID within the ML probe request that is invoked as part
    of scan request. Add support to configure MLD ID in
    scan start params to FW.
    
    Change-Id: I4a6debf6c3ec830fbe3aee1d3aa60ad7218cbcd6
    CRs-Fixed: 3591596

--- umac/scan/dispatcher/inc/wlan_scan_public_structs.h
+++ umac/scan/dispatcher/inc/wlan_scan_public_structs.h
@@ -1117,6 +1117,7 @@ enum scan_request_type {
  * @num_hint_bssid: number of BSSID hints
  * @hint_s_ssid: short SSID hints
  * @hint_bssid: BSSID hints
+ * @mld_id: MLD ID of the requested BSS within ML probe request
  */
 struct scan_req_params {
 	uint32_t scan_id;
@@ -1217,6 +1218,7 @@ struct scan_req_params {
 	uint32_t num_hint_bssid;
 	struct hint_short_ssid hint_s_ssid[WLAN_SCAN_MAX_HINT_S_SSID];
 	struct hint_bssid hint_bssid[WLAN_SCAN_MAX_HINT_BSSID];
+	uint8_t mld_id;
 };
 
 /**
