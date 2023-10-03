16442fd702a5d745275c8499a07c0be27e5a7349
Amit Mehta <quic_amitmeht@quicinc.com>
2023-10-03 02:57:09 -0700

    qcacmn: Add changes to account for encryption header size
    This reverts Change-Id: I521b0990fe9e5746a8c8cfb29de7064cf51d0687
    (qcacmn: Add fix for encryption fragment ping)
    
    Original change has removed logic to Account for encryption
    header size during 802.11 header decapsulation. Which is resulting
    in DHCP failure in fragmented DHCP frames, as N/W stack is dropping DHCP
    offer due to invalid payload.
    
    Revert back to account for encryption header size during
    802.11 header decapsulation
    
    Change-Id: I246e19bf7637d3b66b5d0b3648c76d8765502ac6
    CRs-Fixed: 3627678
    (cherry picked from commit 70b27b1e0802c8e2c77f176030866f077c5c49c7)

--- dp/wifi3.0/dp_rx_defrag.c
+++ dp/wifi3.0/dp_rx_defrag.c
@@ -1500,6 +1500,8 @@ QDF_STATUS dp_rx_defrag(struct dp_txrx_peer *txrx_peer, unsigned int tid,
 			cur = tmp_next;
 		}
 
+		/* If success, increment header to be stripped later */
+		hdr_space += dp_f_ccmp.ic_header;
 		break;
 
 	case cdp_sec_type_wep40:
@@ -1518,6 +1520,8 @@ QDF_STATUS dp_rx_defrag(struct dp_txrx_peer *txrx_peer, unsigned int tid,
 			cur = tmp_next;
 		}
 
+		/* If success, increment header to be stripped later */
+		hdr_space += dp_f_wep.ic_header;
 		break;
 	case cdp_sec_type_aes_gcmp:
 	case cdp_sec_type_aes_gcmp_256:
@@ -1533,6 +1537,7 @@ QDF_STATUS dp_rx_defrag(struct dp_txrx_peer *txrx_peer, unsigned int tid,
 			cur = tmp_next;
 		}
 
+		hdr_space += dp_f_gcmp.ic_header;
 		break;
 	default:
 		break;
