6934c6e4e0cd1b990391ce6ba487e692e8930ddc
Rahul Gusain <quic_rgusain@quicinc.com>
2024-09-06 17:25:24 +0530

    qcacmn: Add feature flags for NAN USD offload
    Add a separate feature flag to indicate support for NAN USD offload
    feature.
    
    Change-Id: Ie620c2898219df4cf488d69b173d32825302f7d1
    CRs-Fixed: 3917320

--- os_if/linux/qca_vendor.h
+++ os_if/linux/qca_vendor.h
@@ -5141,6 +5141,9 @@ enum qca_wlan_vendor_attr_nd_offload {
  *	strip/modify the RSN Selection element indicated in connect request
  *	elements or add that element if none was provided based on the BSS
  *	selected by the driver.
+ * @QCA_WLAN_VENDOR_FEATURE_NAN_USD_OFFLOAD: Flag indicates that the driver
+ *	supports Unsynchronized Service Discovery to be offloaded to it.
+ *
  * @NUM_QCA_WLAN_VENDOR_FEATURES: Number of assigned feature bits
  */
 enum qca_wlan_vendor_features {
@@ -5169,6 +5172,7 @@ enum qca_wlan_vendor_features {
 	QCA_WLAN_VENDOR_FEATURE_AP_ALLOWED_FREQ_LIST = 22,
 	QCA_WLAN_VENDOR_FEATURE_ENHANCED_AUDIO_EXPERIENCE_OVER_WLAN = 23,
 	QCA_WLAN_VENDOR_FEATURE_RSN_OVERRIDE_STA = 25,
+	QCA_WLAN_VENDOR_FEATURE_NAN_USD_OFFLOAD = 26,
 	NUM_QCA_WLAN_VENDOR_FEATURES /* keep last */
 };
 
