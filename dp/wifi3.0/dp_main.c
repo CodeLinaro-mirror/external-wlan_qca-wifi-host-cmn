arter97/caf_qca-wifi-host-cmn

qcacmn: Replace kzalloc/kfree with vmalloc/vfree for vdev
Replace malloc/free with valloc/vfree for vdev
to avoid physical continuous memory alloc failure
in the LE platform.

vdev_context_size=38400 from dp_vdev_attach_wifi3().

Change-Id: I9d11690a5d3f030b8bb4eb9b8a22c387ac0453b2
CRs-Fixed: 3849687

dp/wifi3.0/dp_main.c
@@ -4690,12 +4690,12 @@ static QDF_STATUS dp_vdev_attach_wifi3(struct cdp_soc_t *cdp_soc,
 
 	vdev_context_size =
 		soc->arch_ops.txrx_get_context_size(DP_CONTEXT_TYPE_VDEV);
-	vdev = qdf_mem_malloc(vdev_context_size);
+	vdev = qdf_mem_common_alloc(vdev_context_size);
 
 	if (!pdev) {
 		dp_init_err("%pK: DP PDEV is Null for pdev id %d",
 			    cdp_soc, pdev_id);
-		qdf_mem_free(vdev);
+		qdf_mem_vfree(vdev);
 		goto fail0;
 	}
 
@@ -6338,7 +6338,7 @@ free_vdev:
 				     vdev);
 	wlan_minidump_remove(vdev, sizeof(*vdev), soc->ctrl_psoc,
 			     WLAN_MD_DP_VDEV, "dp_vdev");
-	qdf_mem_free(vdev);
+	qdf_mem_vfree(vdev);
 	vdev = NULL;
 
 	if (vdev_delete_cb)

CodeLinaro-mirror/external-wlan_qca-wifi-host-cmn

dp/wifi3.0/dp_main.c
@@ -4924,12 +4924,12 @@ static QDF_STATUS dp_vdev_attach_wifi3(struct cdp_soc_t *cdp_soc,
 
 	vdev_context_size =
 		soc->arch_ops.txrx_get_context_size(DP_CONTEXT_TYPE_VDEV);
-	vdev = qdf_mem_malloc(vdev_context_size);
+	vdev = qdf_mem_common_alloc(vdev_context_size);
 
 	if (!pdev) {
 		dp_init_err("%pK: DP PDEV is Null for pdev id %d",
 			    cdp_soc, pdev_id);
-		qdf_mem_free(vdev);
+		qdf_mem_common_free(vdev);
 		goto fail0;
 	}
 
@@ -6635,7 +6635,7 @@ free_vdev:
 				     vdev);
 	wlan_minidump_remove(vdev, sizeof(*vdev), soc->ctrl_psoc,
 			     WLAN_MD_DP_VDEV, "dp_vdev");
-	qdf_mem_free(vdev);
+	qdf_mem_common_free(vdev);
 	vdev = NULL;
 
 	if (vdev_delete_cb)