arter97/caf_qca-wifi-host-cmn

qcacmn: Set proper metadata for IPA and EasyMesh enabled case
This change sets proper metadata for IPA and EasyMesh enabled case, if
enabled IPA_WDI3_TX_TWO_PIPES.

Change-Id: Ia82e9a6e8272c5ee09c3f0093579f6f8bf581652
CRs-Fixed: 3728183

dp/wifi3.0/dp_ipa.c
@@ -888,6 +888,25 @@ static void dp_ipa_set_pipe_db(struct dp_ipa_resources *res,
 		QDF_IPA_WDI_CONN_OUT_PARAMS_TX_UC_ALT_DB_PA(out);
 }
 
+#ifdef IPA_WDS_EASYMESH_FEATURE
+static void dp_ipa_setup_iface_session_id(qdf_ipa_wdi_reg_intf_in_params_t *in,
+					  uint8_t session_id)
+{
+	bool is_2g_iface = session_id & IPA_SESSION_ID_SHIFT;
+
+	session_id = session_id >> IPA_SESSION_ID_SHIFT;
+	dp_debug("session_id %u is_2g_iface %d", session_id, is_2g_iface);
+
+	if (ucfg_ipa_is_wds_enabled()) {
+		QDF_IPA_WDI_REG_INTF_IN_PARAMS_META_DATA(in) =
+						htonl(session_id);
+	} else {
+		QDF_IPA_WDI_REG_INTF_IN_PARAMS_META_DATA(in) =
+						htonl(session_id << 16);
+	}
+	QDF_IPA_WDI_REG_INTF_IN_PARAMS_IS_TX1_USED(in) = is_2g_iface;
+}
+#else
 static void dp_ipa_setup_iface_session_id(qdf_ipa_wdi_reg_intf_in_params_t *in,
 					  uint8_t session_id)
 {
@@ -899,6 +918,7 @@ static void dp_ipa_setup_iface_session_id(qdf_ipa_wdi_reg_intf_in_params_t *in,
 	QDF_IPA_WDI_REG_INTF_IN_PARAMS_META_DATA(in) = htonl(session_id << 16);
 	QDF_IPA_WDI_REG_INTF_IN_PARAMS_IS_TX1_USED(in) = is_2g_iface;
 }
+#endif
 
 static void dp_ipa_tx_comp_ring_init_hp(struct dp_soc *soc,
 					struct dp_ipa_resources *res)

CodeLinaro-mirror/external-wlan_qca-wifi-host-cmn

dp/wifi3.0/dp_ipa.c
@@ -1211,6 +1211,24 @@ static void dp_ipa_setup_iface_session_id(qdf_ipa_wdi_reg_intf_in_params_t *in,
 	QDF_IPA_WDI_REG_INTF_IN_PARAMS_META_DATA(in) = htonl(session_id);
 	QDF_IPA_WDI_REG_INTF_IN_PARAMS_IS_TX1_USED(in) = is_2g_iface;
 }
+#elif IPA_WDS_EASYMESH_FEATURE
+static void dp_ipa_setup_iface_session_id(qdf_ipa_wdi_reg_intf_in_params_t *in,
+					  uint8_t session_id)
+{
+	bool is_2g_iface = session_id & IPA_SESSION_ID_SHIFT;
+
+	session_id = session_id >> IPA_SESSION_ID_SHIFT;
+	dp_debug("session_id %u is_2g_iface %d", session_id, is_2g_iface);
+
+	if (ucfg_ipa_is_wds_enabled()) {
+		QDF_IPA_WDI_REG_INTF_IN_PARAMS_META_DATA(in) =
+						htonl(session_id);
+	} else {
+		QDF_IPA_WDI_REG_INTF_IN_PARAMS_META_DATA(in) =
+						htonl(session_id << 16);
+	}
+	QDF_IPA_WDI_REG_INTF_IN_PARAMS_IS_TX1_USED(in) = is_2g_iface;
+}
 #else
 static void dp_ipa_setup_iface_session_id(qdf_ipa_wdi_reg_intf_in_params_t *in,
 					  uint8_t session_id)