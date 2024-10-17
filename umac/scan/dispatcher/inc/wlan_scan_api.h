c2974a5bcd9f953802e21254e756fe494440e7ed
Surya Prakash Sivaraj <quic_suryapra@quicinc.com>
2024-10-17 18:07:30 +0530

    qcacmn: API to get scan entry based on crypto
    Add an API to get the scan entry based on the bssid
    and the crypto params of the vdev.
    
    When RSNO is being used, the highest supported RSN(O)
    IE need not necessarily be the RSN parameter of the
    connection. Therefore, always filter out based on the
    crypto to get the proper security_info of the scan entry.
    
    Change-Id: Ida69cf54b7671fcf534982bf44d78728c55b2008
    CRs-Fixed: 3883379

--- umac/scan/dispatcher/inc/wlan_scan_api.h
+++ umac/scan/dispatcher/inc/wlan_scan_api.h
@@ -55,6 +55,20 @@ wlan_scan_get_scan_entry_by_mac_freq(struct wlan_objmgr_pdev *pdev,
 				     struct qdf_mac_addr *bssid,
 				     uint16_t freq);
 
+/*
+ * wlan_scan_entry_from_bssid_and_crypto() - API to get scan entry
+ * from the bssid and crypto params of the vdev
+ * @pdev: pointer to pdev object
+ * @bssid: pointer to mac addr
+ * @vdev_id: vdev id
+ *
+ * Return: scan entry if found, else NULL
+ */
+struct scan_cache_entry *
+wlan_scan_entry_from_bssid_and_crypto(struct wlan_objmgr_pdev *pdev,
+				      struct qdf_mac_addr *bssid,
+				      uint8_t vdev_id);
+
 /**
  * wlan_scan_cfg_set_active_2g_dwelltime() - API to set scan active 2g dwelltime
  * @psoc: pointer to psoc object
