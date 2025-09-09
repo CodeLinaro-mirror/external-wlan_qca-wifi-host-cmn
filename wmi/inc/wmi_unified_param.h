fb688968b972e8f3270b1b4393de2b2f1da475a7
Kiran Kumar Lokere <klokere@qti.qualcomm.com>
2025-09-09 00:24:54 -0700

    qcacmn: Add support to ignore TWT bcast id in delete req
    Add support to ignore TWT broadcast id in delete request for
    test purpose.
    
    CRs-Fixed: 4286408
    Change-Id: Id18e5e4fbd3380847da310012464050d33e211e5

--- wmi/inc/wmi_unified_param.h
+++ wmi/inc/wmi_unified_param.h
@@ -3310,6 +3310,8 @@ struct set_fwtest_params {
  *                        of 6g connection
  * @WFA_CONFIG_OFDMA: configure driver/firmware to force HE trigger to EHT STA
  * @WFA_CONFIG_ML: configure driver/firmware with Multi-link params
+ * @WFA_IGNORE_TWT_BCAST_ID: configure driver to ignore TWT BCAST_ID in
+ * 			     TWT delete req
  */
 enum wfa_test_cmds {
 	WFA_CONFIG_RXNE,
@@ -3319,7 +3321,8 @@ enum wfa_test_cmds {
 	WFA_FILS_DISCV_FRAMES,
 	WFA_IGNORE_H2E_RSNXE,
 	WFA_CONFIG_OFDMA,
-	WFA_CONFIG_ML
+	WFA_CONFIG_ML,
+	WFA_IGNORE_TWT_BCAST_ID
 };
 
 /**
