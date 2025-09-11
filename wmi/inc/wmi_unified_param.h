2a827a38cf3cf95c38b082714f2eefb35c5a883c
Srinivas Dasari <dasaris@qti.qualcomm.com>
2025-09-11 17:48:14 +0530

    qcacmn: Parse FW DAR support service cap
    Firmware advertises a service cap CTRL_PATH_STA_DAR_STATS_SUPPORT
    when it supports DAR radio stats and control plane stats.
    Parse the same and cache it.
    
    Change-Id: If54e2f46a06d2ed56ba626614ad4b8473b8301c6
    CRs-Fixed: 4288278

--- wmi/inc/wmi_unified_param.h
+++ wmi/inc/wmi_unified_param.h
@@ -6864,6 +6864,7 @@ typedef enum {
 	wmi_service_twt_p2p_go_concurrency_support,
 	wmi_service_sta_twt_stats_ext,
 	wmi_service_scc_tpc_power_support,
+	wmi_service_sta_dar_stats_support,
 	wmi_services_max,
 } wmi_conv_service_ids;
 #define WMI_SERVICE_UNAVAILABLE 0xFFFF
--- wmi/src/wmi_unified_tlv.c
+++ wmi/src/wmi_unified_tlv.c
@@ -24737,6 +24737,8 @@ static void populate_tlv_events_id(WMI_EVT_ID *event_ids)
 #ifdef FEATURE_WLAN_TX_POWERBOOST
 	event_ids[wmi_pdev_power_boost_eventid] = WMI_PDEV_POWER_BOOST_EVENTID;
 #endif
+	wmi_service[wmi_service_sta_dar_stats_support] =
+				WMI_SERVICE_CTRL_PATH_STA_DAR_STATS_SUPPORT;
 }
 
 #ifdef WLAN_FEATURE_LINK_LAYER_STATS
