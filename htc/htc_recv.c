431d7d880353aa17b86b02991eff11b67fd81b82
jianfzh <jianfzh@qti.qualcomm.com>
2026-07-14 22:32:06 -0700

    qcacmn: Force fw crash on invalid HTC messages
    If the host receives erroneous HTC messages,
    force an fw crash instead of crashing the host.
    
    Change-Id: Ic93eb4d1f8b8803f37ab4f0c9dd9fea2ffe6ef0a

--- htc/htc_recv.c
+++ htc/htc_recv.c
@@ -436,7 +436,7 @@ QDF_STATUS htc_rx_completion_handler(void *Context, qdf_nbuf_t netbuf,
 						sizeof(HTC_FRAME_HDR),
 						"BAD HTC Header");
 					print_hex_dump(KERN_ERR, "HTC msg buffer:", DUMP_PREFIX_NONE, 16, 1, netdata, netlen, true);
-					QDF_BUG(false);
+					cnss_force_fw_assert(NULL);
 					break;
 				}
 
